bbc7886460a733490b19e92fb65056b60dee265e
Philipp Hock <philipp.hock@uni-ulm.de>
2013-10-25 16:24:52 +0200

    init

---
+++ src/de/uniulm/bagception/bluetoothservermessengercommunication/MessengerConstants.java
@@ -0,0 +1,10 @@
+package de.uniulm.bagception.bluetoothservermessengercommunication;
+
+public class MessengerConstants {
+
+	public static final int MESSAGE_BUNDLE_SEND=0;
+	public static final int MESSAGE_BUNDLE_RECV=1;
+	public static final int MESSAGE_BUNDLE_CONNECTED_WITH_DEVICE=2;
+	public static final int MESSAGE_BUNDLE_DISCONNECTED_WITH_DEVICE=3;
+	
+}
