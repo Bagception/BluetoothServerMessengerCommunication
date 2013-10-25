4dd9a7737c369c00cfc7b1242685a6582bbb93f1
Philipp Hock <philipp.hock@uni-ulm.de>
2013-10-25 17:40:48 +0200

    autogenerated commit

--- src/de/uniulm/bagception/bluetoothservermessengercommunication/MessengerConstants.java
+++ src/de/uniulm/bagception/bluetoothservermessengercommunication/MessengerConstants.java
@@ -3,5 +3,6 @@ package de.uniulm.bagception.bluetoothservermessengercommunication;
 public class MessengerConstants {
 
 	public static final int MESSAGE_BUNDLE_MESSAGE=0;
+	public static final int MESSAGE_RESEND_SERVER_INFO_MESSAGE=1;
 	
 }
