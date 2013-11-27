d2e5c3b4b6d05e55db5389ddd3136fc22529bbe5
Philipp Hock <philipp.hock@uni-ulm.de>
2013-11-27 19:49:59 +0100

    client messaging not via broadcasts

--- src/de/uniulm/bagception/bluetooth/CheckReachableCallback.java
+++
@@ -1,9 +0,0 @@
-package de.uniulm.bagception.bluetooth;
-
-import android.bluetooth.BluetoothDevice;
-
-public interface CheckReachableCallback {
-
-	public void isReachable(BluetoothDevice device,boolean reachable);
-	
-}
